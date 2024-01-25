33c660fc6b4b2fda30f7dc0d130a2023ca591381
Amarjeet Chaudhary <siyhcompany@gmail.com>
2024-01-25 15:53:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".ri-menu-line").addEventListener("click", function(){
+    document.querySelector(".main-nav").style.top = "0%";
+})
+
+document.querySelector(".ri-close-circle-line").addEventListener("click", function(){
+    document.querySelector(".main-nav").style.top = "-100%";
+})
